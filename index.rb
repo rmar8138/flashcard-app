9912825b8c992d07ad979be9879175619494aee2
rmar8138 <ragan.martinez@live.com>
2019-09-03 12:25:54 +1000

    Initial commit

---
+++ index.rb
@@ -0,0 +1,11 @@
+
+
+# WELCOME SCREEN
+puts "\n\n\n"
+puts "Welcome to the terminal flash card app!"
+puts "\n"
+puts "Please select an option by typing in the number to the left:"
+puts "\n\n"
+puts "(1) Review\n(2) Add deck\n(3) Edit deck\n(4) Settings"
+puts "\n\n\n"
+
