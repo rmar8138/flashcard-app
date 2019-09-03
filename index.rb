72059e2f1a814c0767c2cc46e8cf397228dafa3a
rmar8138 <ragan.martinez@live.com>
2019-09-03 13:04:50 +1000

    Add basic card making functionality

--- index.rb
+++ index.rb
@@ -1,12 +1,10 @@
 require_relative "./classes/Deck"
 
-def new_deck
+def add_deck
   puts "Please enter a title for the deck"
   title = gets.chomp
 
-  new_deck = Deck.new(title)
-  p new_deck.title
-  p new_deck.cards
+  Deck.new(title)
 end
 
 welcome_menu_open = true
@@ -15,7 +13,7 @@ while welcome_menu_open
   # WELCOME SCREEN
   puts "\n\n\n"
   puts "Welcome to the terminal flash card app!"
-  puts "\n"
+  puts "\n\n"
   puts "Please select an option by typing in the number to the left:"
   puts "\n\n"
   puts "(1) Review\n(2) Add deck\n(3) Edit deck\n(4) Settings\n(5) Exit"
@@ -31,7 +29,37 @@ while welcome_menu_open
     welcome_menu_open = false
   when "2"
     system "clear"
-    new_deck
+    new_deck = add_deck
+    
+    finished_adding_cards = false
+    until finished_adding_cards
+      puts "Card #{new_deck.cards.length + 1}"
+      puts "\n\n"
+      puts "Enter the question"
+      question = gets.chomp
+      puts "Enter the answer"
+      answer = gets.chomp
+
+      new_deck.add_card({ question: question, answer: answer })
+
+      add_another_card_prompt_open = true
+      while add_another_card_prompt_open
+        puts "Add another card? (y/n)"
+        input = gets.chomp
+        
+        case input
+        when "y"
+          add_another_card_prompt_open = false
+        when "n"
+          add_another_card_prompt_open = false
+          finished_adding_cards = true
+        else
+          puts "Invalid input"
+        end
+      end
+      
+      p new_deck.cards
+    end
 
     welcome_menu_open = false
   when "3"
