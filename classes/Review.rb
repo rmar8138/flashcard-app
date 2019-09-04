e2e2a6b503ebef1e4ad61528cd7b24bba8b7bc05
rmar8138 <ragan.martinez@live.com>
2019-09-04 12:47:38 +1000

    Add basic review functionality without subsequent reviews of failed cards

--- classes/Review.rb
+++ classes/Review.rb
@@ -8,20 +8,86 @@ class Review
   end
 
   def start_review
-    puts "Deck: #{@deck[:title]}"
-    puts "\n"
-    puts "To exit the review at any time, hit the esc key and press enter/return"
-    
-    review_in_session = true
+    shuffled_deck = @deck[:cards].shuffle
+    review_after = []
 
-    while review_in_session
-      
+    while @current_card < shuffled_deck.length
+      system "clear"
 
+      puts "Deck: #{@deck[:title]}"
+      puts "\n"
+      puts "To exit the review at any time, hit the esc key and press enter/return"
+      puts "\n\n"
+      puts "Question: #{shuffled_deck[@current_card][:question]}"
+      puts "\n\n"
+      puts "Would you like to show answer or skip? Enter number to the left"
+      puts "(1) Show answer\n(2) Skip card"
+      input = gets.chomp
+
+      system "clear"
+        case input
+        when "1"
+        user_viewing_answer = true
+        while user_viewing_answer
+          puts "Deck: #{@deck[:title]}"
+          puts "\n"
+          puts "To exit the review at any time, hit the esc key and press enter/return"
+          puts "\n\n"
+          puts "Question: #{shuffled_deck[@current_card][:question]}"
+          puts "\n\n"
+          puts "Answer: #{shuffled_deck[@current_card][:answer]}"
+          puts "\n\n"
+          puts "Enter the number to the left of the appropriate outcome"
+          puts "\n"
+          puts "(1) Correct\n(2) Incorrect"
+          input = gets.chomp
+          case input
+          when "1"
+            @score += 1
+            user_viewing_answer = false
+            next
+          when "2"
+            # add card to review_after deck #
+            review_after.push(shuffled_deck[@current_card])
+            user_viewing_answer = false
+            next
+          else
+            system "clear"
+            puts "Invalid input"
+            next
+          end
+        end
+
+        # NEXT CARD #
+        @current_card += 1
+        system "clear"
+        next
+      when "2"
+        # Move card to back of pile
+        skipped_card = shuffled_deck.slice!(@current_card)
+        shuffled_deck.push(skipped_card)
+        system "clear"
+        next
+      else
+        system "clear"
+        puts "Invalid input"
+        next
+      end
+    end
+
+    # IF review_after has cards, shuffled deck = review_after and review these, repeat.
+    # else, show statistics
+
+    if review_after.length > 0
+      p review_after
+    else
+      show_statistics
     end
   end
 
   def show_statistics
-
+    puts "Statistics!"
+    puts "Score: #{@score}/#{@deck[:cards].length}"
   end
 end
 
